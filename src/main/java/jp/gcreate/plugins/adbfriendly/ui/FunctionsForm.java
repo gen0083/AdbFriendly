9a09e37ace59e2c3a1fac187a1dbcfc0c5f66146
Gen <archiherewego@gmail.com>
2016-04-22 22:36:44 +0900

    add validation test

--- src/main/java/jp/gcreate/plugins/adbfriendly/ui/FunctionsForm.java
+++ src/main/java/jp/gcreate/plugins/adbfriendly/ui/FunctionsForm.java
@@ -19,14 +19,22 @@ package jp.gcreate.plugins.adbfriendly.ui;
 
 import com.android.ddmlib.AndroidDebugBridge;
 import com.android.ddmlib.IDevice;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.DialogEarthquakeShaker;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.ValidationInfo;
 import jp.gcreate.plugins.adbfriendly.adb.AdbConnector;
 import jp.gcreate.plugins.adbfriendly.funciton.FriendlyFunctions;
 import jp.gcreate.plugins.adbfriendly.funciton.FunctionsCallback;
 import jp.gcreate.plugins.adbfriendly.funciton.FunctionsManager;
+import jp.gcreate.plugins.adbfriendly.util.Logger;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FunctionsForm extends DialogWrapper
         implements FunctionsCallback, AndroidDebugBridge.IDeviceChangeListener {
@@ -48,7 +56,21 @@ public class FunctionsForm extends DialogWrapper
         setListenerOnLaunch();
 
         connectedDevicesModel = new DefaultListModel();
-        buttonRolling.setAction(new DialogWrapperExitAction("hoge", DialogWrapper.CLOSE_EXIT_CODE));
+        buttonRolling.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Logger.d(this, "button rolling clicked.");
+
+                ValidationInfo info = doValidate();
+                if (info != null) {
+                    DialogEarthquakeShaker.shake((JDialog)getPeer().getWindow());
+//                    startTrackingValidation();
+                    initValidation();
+                    return;
+                }
+                Notifications.Bus.notify(new Notification("test", "do action", "pass the validation", NotificationType.INFORMATION));
+            }
+        });
         devicesList.setCellRenderer(new DevicesListRenderer());
         init();
         bindDevicesToList();
@@ -83,9 +105,25 @@ public class FunctionsForm extends DialogWrapper
     }
 
     private void checkRunnningTaskExisit() {
-
+        FriendlyFunctions currentFunction = FunctionsManager.INSTANCE.getRunningFunctionOrNull();
+        boolean running = currentFunction != null;
+        // If running functions now then set disable buttons which to run functions.
+        buttonRolling.setEnabled(running);
     }
 
+    @Override
+    protected ValidationInfo doValidate() {
+        Logger.d(this, "validation start");
+        if (devicesList.getSelectedIndex() == -1) {
+            return new ValidationInfo("Select a target device.", devicesList);
+        }
+        return null;
+    }
+
+    /**
+     * When device status changed, these callbacks are called.
+     * @param device
+     */
     @Override
     public void deviceConnected(IDevice device) {
         bindDevicesToList();
@@ -101,19 +139,23 @@ public class FunctionsForm extends DialogWrapper
         bindDevicesToList();
     }
 
+    /**
+     * If functions are finished then these callbacks are called from FunctionsManager.
+     * The callbacks notify that FunctionsManager can start a next function.
+     */
     @Override
     public void onDone() {
-
+        checkRunnningTaskExisit();
     }
 
     @Override
     public void onErrored() {
-
+        checkRunnningTaskExisit();
     }
 
     @Override
     public void onCancelled() {
-
+        checkRunnningTaskExisit();
     }
 
     public class DoFunctionsAction{
